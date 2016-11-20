9194f807a8630fd5580d581241d1c3389b39de88
WalterNavarrete <walter.navarrete@outlook.com>
2016-11-20 16:49:46 -0300

    Entrega TP4

---
+++ TP4_WalterNavarrete/Navegador TP-4/Hilo/Descargador.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+    public class Descargador
+    {
+        private string html;
+        private Uri direccion;
+
+        //Se agregan los delegados
+        public delegate void EventProgress(int progreso);
+        public delegate void EventEnd(string html);
+        //Los eventos asociado a los delegados
+        public event EventProgress eventProgress;
+        public event EventEnd eventEnd;
+
+
+        public Descargador(Uri direccion)
+        {
+            this.html = "";
+            //inicializo la direccion
+            this.direccion = direccion;
+        }
+
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.DownloadProgressChanged += WebClientDownloadProgressChanged;
+                cliente.DownloadStringCompleted += WebClientDownloadCompleted;
+
+                cliente.DownloadStringAsync(this.direccion);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.eventProgress(e.ProgressPercentage);
+        }
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            this.html = e.Result;
+            this.eventEnd(this.html);
+        }
+    }
+}
---
+++ TP4_WalterNavarrete/Navegador TP-4/Navegador/frmHistorial.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Navegador
+{
+    public partial class frmHistorial : Form
+    {
+        public const string ARCHIVO_HISTORIAL = "historico.dat";
+
+        public frmHistorial()
+        {
+            InitializeComponent();
+        }
+
+        private void frmHistorial_Load(object sender, EventArgs e)
+        {
+
+            Archivos.Texto archivos = new Archivos.Texto(frmHistorial.ARCHIVO_HISTORIAL);
+
+            List<string> _historial;
+
+            if (archivos.leer(out _historial))
+            {
+                lstHistorial.DataSource = _historial;
+            }
+            else
+            {
+                MessageBox.Show("Ocurrio un Error al leer el Archivo");
+            }
+
+        }
+    }
+}
